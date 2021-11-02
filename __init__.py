7c12a94d70bc07d620419d6cefaad0d2f5c039af
systemb4 <lukasnitzsche@yahoo.com>
2021-11-01 21:47:12 -0500

    clean up for tutorial

--- __init__.py
+++ __init__.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/LukasNitzsche')
-def LukasNitzsche():
-    return render_template('LukasNitzsche/index.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
