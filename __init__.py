cd9ae1f701e32e8c249e7b40fbfd89449774d008
23LNitzsche <92123150+23LNitzsche@users.noreply.github.com>
2021-11-01 22:01:09 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/JoeStudent')
+    def JoeStudent():
+        return render_template('JoeStudent/index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
