d2019c27297c1870778dfd439b6bc1ddd3e6fe90
systemb4 <lukasnitzsche@yahoo.com>
2021-11-22 17:58:03 -0600

    completly change website

--- __init__.py
+++ __init__.py
@@ -6,13 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/JoeStudent')
-def JoeStudent():
-    return render_template('JoeStudent/index.html')
-
 @app.route('/tutorial_video')
 def tutorialVideo():
     return render_template('tutorial_video.html')
+
+@app.route('/member_projects')
+def member_projects():
+    return render_template('member_projects.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/JoeStudent')
+def JoeStudent():
+    return render_template('JoeStudent/index.html')
 
 @app.route('/Francis')
 def Francis():
